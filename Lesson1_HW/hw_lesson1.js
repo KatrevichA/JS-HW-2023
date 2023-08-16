01f8af1a599035395e0689f7f8544a783ebcbccd
KatrevichA <katrevich24@gmail.com>
2023-08-16 17:00:03 +0300

    add stepHW to hw_lesson1.js

---
+++ Lesson1_HW/hw_lesson1.js
@@ -0,0 +1,40 @@
+// 1 step //
+let s = 'hello';
+console.log(s);
+let s1 = 'owu';
+console.log(s1);
+let s2 = 'com';
+console.log(s2);
+let s3 = 'ua';
+console.log(s3);
+let n =1;
+console.log(n);
+let n1 =10;
+console.log(n1);
+let n2 = -999;
+console.log(n2);
+let n3 = 123;
+console.log(n3);
+let n4 = 3.14;
+console.log(n4);
+let n5 = 2.7;
+console.log(n5);
+let n6 = 16;
+console.log(n6);
+let bt = true;
+console.log(bt);
+let bf = false;
+console.log(bf);
+//2 step //
+let firstName = "Anastasia";
+let middleName = "Muravlova"
+let lastName = "Bogdanivna"
+let person = firstName+' ' +middleName+' '+lastName;
+console.log(person);
+//3 step//
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
